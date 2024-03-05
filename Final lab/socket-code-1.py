9b32815e966cf603fc19a48f0e9be9ef3428f532
CedricttChartier <43191516+requiem85@users.noreply.github.com>
2024-03-04 21:30:50 -0800

    ...

--- Final lab/socket-code-1.py
+++ Final lab/socket-code-1.py
@@ -54,7 +54,7 @@ while True:
         #    mininet]
    elif user_input == "/bridge":
     #    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        data2 = "BRIDGE\r\n" + "clientID: ced\r\n" + "\r\n"
+        data2 = "BRIDGE\r\nclientID: ced\r\n\r\n"
         # s.connect((server_ip, int(server_port)))
         print(data2)
         s.send(data2.encode())
