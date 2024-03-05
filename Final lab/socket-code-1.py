89da7a1329dc57c11e5786ac4cc2e8c8120af0da
CedricttChartier <43191516+requiem85@users.noreply.github.com>
2024-03-04 21:47:49 -0800

    ...

--- Final lab/socket-code-1.py
+++ Final lab/socket-code-1.py
@@ -11,61 +11,69 @@ args = parser.parse_args()
 
 # Validate port and server arguments
 if not 1024 <= args.port <= 65535:
-   print("Invalid port number. Must be between 1024 and 65535.")
-   exit(1)
+    print("Invalid port number. Must be between 1024 and 65535.")
+    exit(1)
 
 try:
     server_ip, server_port = args.server.split(":")
     # server_port = args.port #server port
     # server_ip = args.server #server ip
 except ValueError:
-   print("Invalid server argument. Format: IP:Port")
-   exit(1)
+    print("Invalid server argument. Format: IP:Port")
+    exit(1)
 
 s = socket(AF_INET, SOCK_STREAM)
 
+valid = ["/id", "/register", "/bridge", "/chat", "/quit"]
+
 print(server_ip, int(server_port))
-s.connect((server_ip, int(server_port)))
 # Main loop
 while True:
-   user_input = input("Enter command: ").lower()
-   print(user_input)
+    user_input = input("Enter command: ").lower()
+    print(user_input)
+    if user_input not in valid:
+        print("Invalid command.")
+        continue
+    else:
+        s.connect((server_ip, int(server_port)))
 
-   if user_input == "/id":
-       print("Your ID:", args.id)
+    if user_input == "/id":
+        print("Your ID:", args.id)
 
-   elif user_input == "/register":
-    #    with socket(AF_INET, SOCK_STREAM) as s:
+    elif user_input == "/register":
+        #    with socket(AF_INET, SOCK_STREAM) as s:
         #    s.bind(("127.0.0.1", server_port))
         #    s.listen(1)
         #    s.settimeout(5)
-         #   server_ip, server_port = args.server.split(":")
-         #   print(server_ip, int(server_port))
+        #   server_ip, server_port = args.server.split(":")
+        #   print(server_ip, int(server_port))
         #    s.connect((server_ip, int(server_port)))
-           sys.stdout.write("yo yo you")
-           print("test1")
-           data = "REGISTER\r\n" + "clientID: {}\r\n".format(args.id) + "IP: {}\r\n".format(server_ip) + "Port: {}\r\n".format(server_port) + "\r\n"
-           print(data)
-           s.send(data.encode())
-           print("test2")
-           response = s.recv(1024)
-           print(response.decode())
-        #    s.close()
-        #    mininet]
-   elif user_input == "/bridge":
-    #    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        sys.stdout.write("yo yo you")
+        print("test1")
+        data = (
+            "REGISTER\r\n"
+            + "clientID: {}\r\n".format(args.id)
+            + "IP: {}\r\n".format(server_ip)
+            + "Port: {}\r\n".format(server_port)
+            + "\r\n"
+        )
+        print(data)
+        s.send(data.encode())
+        print("test2")
+        response = s.recv(1024)
+        print(response.decode())
+    #    s.close()
+    #    mininet]
+    elif user_input == "/bridge":
+        #    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data2 = "BRIDGE\r\nclientID: {}\r\n\r\n".format(args.id)
         # s.connect((server_ip, int(server_port)))
         print(data2)
         s.send(data2.encode())
         response2 = s.recv(1024).decode()
-           # Process the bridge response here
+        # Process the bridge response here
         print("Bridge response:", response2)
 
-   elif user_input == "/exit":
-       break
-
-   else:
-       print("Invalid command.")
+    s.close()
 
 print("Program terminated.")
