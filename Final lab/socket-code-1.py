6e3bfcae2b148b4b720c1c1ee41b5161895dde29
CedricttChartier <43191516+requiem85@users.noreply.github.com>
2024-03-04 21:35:11 -0800

    ...

--- Final lab/socket-code-1.py
+++ Final lab/socket-code-1.py
@@ -58,9 +58,9 @@ while True:
         # s.connect((server_ip, int(server_port)))
         print(data2)
         s.send(data2.encode())
-        # response2 = s.recv(1024).decode()
+        response2 = s.recv(1024).decode()
            # Process the bridge response here
-        # print("Bridge response:", response2)
+        print("Bridge response:", response2)
 
    elif user_input == "/exit":
        break
