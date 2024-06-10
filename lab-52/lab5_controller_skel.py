03772a333e8540f4fd53be90986a6b0e0a8d3e16
gamer <cedricttchartier@gmail.com>
2024-06-10 03:00:30 -0700

    bigfix!

--- lab-52/lab5_controller_skel.py
+++ lab-52/lab5_controller_skel.py
@@ -1,9 +1,15 @@
+
+#commands to run code
+#rm ~/pox/pox/misc/lab5_controller_skel.py
+#cp lab5_controller_skel.py ~/pox/pox/misc/
+#sudo ~/pox/pox.py log.level --packet=WARN misc.lab5_controller_skel
 from pox.core import core
+
 import pox.openflow.libopenflow_01 as of
+import time
 
 log = core.getLogger()
 
-# Define the IPs of the hosts
 ips = {
     "h101": "128.114.1.101",
     "h102": "128.114.1.102",
@@ -14,130 +20,246 @@ ips = {
     "h203": "128.114.2.203",
     "h204": "128.114.2.204",
     "h_server": "128.114.3.178",
-    "h_trust": "192.47.38.109",
+    "h_trust": "200.20.203.2",
     "h_untrust": "108.35.24.113",
-    "discord": "200.20.50.5"
 }
 
-# Define the switchboard (configuration for routing)
-switchboard = {
-    1: {"subnet": 1, "ports": {41: "128.114.1.101", 42: "128.114.1.102"}},
-    2: {"subnet": 2, "ports": {51: "128.114.2.201", 52: "128.114.2.202"}},
-    3: {"subnet": 3, "ports": {61: "128.114.2.203", 62: "128.114.2.204"}},
-    4: {"subnet": 100, "ports": {18: "128.114.3.178"}},
-    0: {"subnet": None, "ports": {321: "200.20.50.5"}}
-}
+switchboard = [
+    [
+        # switch_id, subnet, port, allowed protocols
+        [1, 1, 1, ["ICMP", "TCP", "UDP"]], # floor 1 switch 1
+        [2, 1, 2, ["ICMP", "TCP", "UDP"]], # floor 1 switch 2
+        [3, 2, 3, ["ICMP", "TCP", "UDP"]], # floor 2 switch 1
+        [4, 2, 4, ["ICMP", "TCP", "UDP"]], # floor 2 switch 2
+        [5, 3, 5, ["ICMP", "TCP", "UDP"]], # datacenter
+        #[4, 100, 4, ["TCP", "UDP"]],
+        # [200, 203, 99, ["TCP"]],
+    ],
+    # ip, dst_port, switch_port
+    {
+        ips["h101"]: (11, 41),
+        ips["h102"]: (12, 42),
+        
+    },
+    {
+        ips["h103"]: (13, 43),
+        ips["h104"]: (14, 44),
+
+    },
+    {
+        ips["h201"]: (21, 51),
+        ips["h202"]: (22, 52),
+    },
+    {
+        ips["h203"]: (23, 53),
+        ips["h204"]: (24, 54),
+        
+
+    },
+    {
+        ips["h_server"]: (17, 18),
+        
+    },
+]
 
 internet = {
-    "192.47.38.109": [1, 2, 3, 100],  # Trusted host access to all subnets
-    "108.35.24.113": []  # Untrusted host access
+    ips["h_trust"]: [1, 2, 3, 4, 5, 6], # which subnet(s) h_trust is allowed to ping
+    ips["h_untrust"]: [1, 2, 3, 4, 5, 6],   
+    
+}
+
+allowed_connections = {
+    1: [1, 2, 3, 4, 5, 6], # subnet 1 is allowed to connect to subnets 1 and 3
+    2: [1, 2, 3, 4, 5, 6],
+    3: [1, 2, 3, 4, 5, 6],
 }
 
 escape_ports = {
-    "128.114.1.101": (41, 1),
-    "128.114.1.102": (42, 1),
-    "128.114.1.103": (43, 1),
-    "128.114.1.104": (44, 1),
-    "128.114.2.201": (51, 2),
-    "128.114.2.202": (52, 2),
-    "128.114.2.203": (53, 2),
-    "128.114.2.204": (54, 2),
-    "128.114.3.178": (18, 4),
-    "192.47.38.109": (200, 3),
-    "108.35.24.113": (201, 0),
-    "200.20.50.5": (321, 0)
+    ips["h101"]: (11, 41),
+    ips["h102"]: (12, 42),
+    ips["h103"]: (13, 43),
+    ips["h104"]: (14, 44),
+    ips["h201"]: (21, 51),
+    ips["h202"]: (22, 52),
+    ips["h203"]: (23, 53),
+    ips["h204"]: (24, 54),
+    ips["h_server"]: (17, 18),
+    ips["h_trust"]: (99, 200),
+    ips["h_untrust"]: (98, 201),
+    # "200.20.50.5": (123, 321)
 }
 
+
 class Routing(object):
+
     def __init__(self, connection):
+        # Keep track of the connection to the switch so that we can
+        # send it messages!
         self.connection = connection
+
+        # This binds our PacketIn event listener
         connection.addListeners(self)
 
     def do_routing(self, packet, packet_in, port_on_switch, switch_id):
+        # port_on_swtich - the port on which this packet was received
+        # switch_id - the switch which received this packet
+
+        # Your code here
+        print(switch_id)
+        print("-------")
+        print(port_on_switch)
+        print("-------")
+
+        # end_port = 0
+
         def accept(end_port):
+            print("accept port", end_port)
             msg = of.ofp_flow_mod()
             msg.match = of.ofp_match.from_packet(packet)
             msg.idle_timeout = 45
             msg.hard_timeout = 45
+            if packet is not None:
+                log.debug(packet)
             msg.actions.append(of.ofp_action_output(port=end_port))
             msg.buffer_id = packet_in.buffer_id
             msg.data = packet_in
             self.connection.send(msg)
-            log.debug("Packet Accepted - Flow Table Installed on Switches")
+            print("Packet Accepted - Flow Table Installed on Switches")
 
         def drop():
+            # Write code for a drop function
+            # Drop packet
+            print("bye")
             msg = of.ofp_flow_mod()
             msg.match = of.ofp_match.from_packet(packet)
             msg.idle_timeout = 30
             msg.hard_timeout = 30
             msg.buffer_id = packet_in.buffer_id
             self.connection.send(msg)
-            log.debug("Packet Dropped - Flow Table Installed on Switches")
+            print("Packet Dropped - Flow Table Installed on Switches")
 
         def checkrule(ip, sw, protocol):
             src = str(ip.srcip)
             dst = str(ip.dstip)
+            print("Source:", ip.srcip)
+            print("Source:", src)
+            print("Dest:", ip.dstip)
+            print("Dest:", dst)
 
+            # Write firewall code
+            # Check if switch_id is not None
             if sw is not None:
+                print("Switch:", sw)
+                # Check if destination IP is in the switchboard table
                 src_subnet = int(src.split(".")[2])
                 dst_subnet = int(dst.split(".")[2])
 
-                if sw not in switchboard:
-                    log.debug("Navigating via core switch")
+                if sw not in [1, 2, 3, 4, 5]:
+                    print("Navigating via coreswitch")
+                    print(src, dst, src_subnet, dst_subnet)
+                    # time.sleep(10)
                     if dst in internet:
+                        print("The internet!")
                         if src_subnet in internet[dst]:
-                            end_port, _ = escape_ports[dst]
+                            _, end_port = escape_ports[dst]
                             accept(end_port)
                         else:
                             drop()
                     else:
-                        log.debug("Navigating within the university")
-                        for row in switchboard.values():
-                            if dst_subnet == row["subnet"] and protocol in ["ICMP", "TCP", "UDP"]:
-                                for port, ip in row["ports"].items():
-                                    if ip == dst:
-                                        accept(port)
-                                        return
-                        drop()
+                        print("the company network!")
+                        if dst_subnet not in allowed_connections[src_subnet]:
+                            drop()
+                        else:
+                            for row in switchboard[0]:
+                                if dst_subnet == row[1] and protocol in row[3] and dst in switchboard[row[0]]:
+                                    end_port = row[2]
+                                    accept(end_port)
+                                    print("End port", end_port)
+                                    return
+                                    # sw = None
+                            # if sw == None:
+                            drop()
                 else:
-                    log.debug(f"Navigating within switch {sw}")
-                    if dst_subnet == switchboard[sw]["subnet"]:
-                        if dst in escape_ports:
-                            end_port, _ = escape_ports[dst]
+                    print("Navigating within switch", sw)
+                    # if dst_subnet == switchboard[0][sw - 1][1]:
+                    if dst in switchboard[sw]:
+                        print("Destination is in same switch")
+                        # for t in switchboard[sw]:
+                        #     tmp_ip, _, tmp_port = t
+                        #     if tmp_ip == ip.dstip:
+                        #         end_port = tmp_port
+                        #         accept()
+                        #         return
+                        # drop()
+                        if dst in escape_ports:# and (dst != "10.0.100.2" or src_subnet == 1):
+                            _, end_port = escape_ports[dst]
                             accept(end_port)
                         else:
                             drop()
                     else:
-                        end_port, _ = switchboard[sw]["ports"].items()[0]
+                        print("Destination is in different switch")
+                        # if protocol in switchboard[0][sw - 1][3]:
+                        end_port = switchboard[0][sw - 1][2]
                         accept(end_port)
+                      
             else:
                 drop()
+            # print("End port", end_port)
+            return
+
+           
 
         icmp = packet.find("icmp")
         tcp = packet.find("tcp")
         udp = packet.find("udp")
-        protocol = "ICMP" if icmp else "TCP" if tcp else "UDP" if udp else None
+        protocol = None
 
-        if not protocol:
+        if icmp:
+            protocol = "ICMP"
+        elif tcp:
+            protocol = "TCP"
+        elif udp:
+            protocol = "UDP"
+        else:
+            print("drop_else")
             drop()
             return
 
         ip = packet.find("ipv4")
-        if ip:
+        print(packet_in)
+        if ip is not None:
+            print(ip, ip.protocol)
+
+            # accept(15)
             checkrule(ip, switch_id, protocol)
+            # accept()
         else:
             drop()
+        return
 
+   
     def _handle_PacketIn(self, event):
-        packet = event.parsed
+        """
+        Handles packet in messages from the switch.
+        """
+        packet = event.parsed  # This is the parsed packet data.
         if not packet.parsed:
             log.warning("Ignoring incomplete packet")
             return
 
-        packet_in = event.ofp
+        print('packet', packet)
+        print('dpid', event.dpid)
+        packet_in = event.ofp  # The actual ofp_packet_in message.
+        print('packet_in', packet_in)
+        print('event.port', event.port)
         self.do_routing(packet, packet_in, event.port, event.dpid)
 
+
 def launch():
+    """
+    Starts the component
+    """
+
     def start_switch(event):
         log.debug("Controlling %s" % (event.connection,))
         Routing(event.connection)
--- lab-52/lab5_topo_skel.py
+++ lab-52/lab5_topo_skel.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+#run this second
+#commands to run code
+#sudo python3 lab5_topo_skel.pyt
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
@@ -13,53 +16,53 @@ class MyTopology(Topo):
     Topo.__init__(self)
     CoreSwitch = self.addSwitch('s0')
     Floor1Switch1 = self.addSwitch('s1')
-    Floor2Switch1 = self.addSwitch('s2')
-    TrustedSwitch = self.addSwitch('s3')
-    DataCenterSwitch = self.addSwitch('s4')
-    Floor1Switch2 = self.addSwitch('s5')
-    Floor2Switch2 = self.addSwitch('s6')
+    Floor1Switch2 = self.addSwitch('s2')
+    Floor2Switch1 = self.addSwitch('s3')
+    Floor2Switch2 = self.addSwitch('s4')
+    DataCenterSwitch = self.addSwitch('s5')
+    # TrustedSwitch = self.addSwitch('s3')
     
     self.addLink(Floor1Switch1, CoreSwitch, port1=1, port2=1)
-    self.addLink(Floor2Switch1, CoreSwitch, port1=2, port2=2)
-    self.addLink(Floor1Switch2, CoreSwitch, port1=3, port2=3)
+    self.addLink(Floor1Switch2, CoreSwitch, port1=2, port2=2)
+    self.addLink(Floor2Switch1, CoreSwitch, port1=3, port2=3)
     self.addLink(Floor2Switch2, CoreSwitch, port1=4, port2=4)
-    self.addLink(TrustedSwitch, CoreSwitch, port1=5, port2=5)
-    self.addLink(DataCenterSwitch, CoreSwitch, port1=6, port2=6)
+    # self.addLink(TrustedSwitch, CoreSwitch, port1=5, port2=5)
+    self.addLink(DataCenterSwitch, CoreSwitch, port1=5, port2=5)
     
     # Floor 1 Hosts
-    h101 = self.addHost('h101', ip='128.114.1.101/24', defaultRoute="h101-eth0", mac='00:00:00:00:00:01')
+    h101 = self.addHost('h101', ip='128.114.1.101/24', defaultRoute="h101-eth11", mac='00:00:00:00:00:01')
     self.addLink(h101, Floor1Switch1, port1=11, port2=41)
-    h102 = self.addHost('h102', ip='128.114.1.102/24', defaultRoute="h102-eth0", mac='00:00:00:00:00:02')
+    h102 = self.addHost('h102', ip='128.114.1.102/24', defaultRoute="h102-eth12", mac='00:00:00:00:00:02')
     self.addLink(h102, Floor1Switch1, port1=12, port2=42)
-    h103 = self.addHost('h103', ip='128.114.1.103/24', defaultRoute="h103-eth0", mac='00:00:00:00:00:03')
+    h103 = self.addHost('h103', ip='128.114.1.103/24', defaultRoute="h103-eth13", mac='00:00:00:00:00:03')
     self.addLink(h103, Floor1Switch2, port1=13, port2=43)
-    h104 = self.addHost('h104', ip='128.114.1.104/24', defaultRoute="h104-eth0", mac='00:00:00:00:00:04')
+    h104 = self.addHost('h104', ip='128.114.1.104/24', defaultRoute="h104-eth14", mac='00:00:00:00:00:04')
     self.addLink(h104, Floor1Switch2, port1=14, port2=44)
 
     # Floor 2 Hosts
-    h201 = self.addHost('h201', ip='128.114.2.201/24', defaultRoute="h201-eth0", mac='00:00:00:00:00:05')
+    h201 = self.addHost('h201', ip='128.114.2.201/24', defaultRoute="h201-eth21", mac='00:00:00:00:00:05')
     self.addLink(h201, Floor2Switch1, port1=21, port2=51)
-    h202 = self.addHost('h202', ip='128.114.2.202/24', defaultRoute="h202-eth0", mac='00:00:00:00:00:06')
+    h202 = self.addHost('h202', ip='128.114.2.202/24', defaultRoute="h202-eth22", mac='00:00:00:00:00:06')
     self.addLink(h202, Floor2Switch1, port1=22, port2=52)
-    h203 = self.addHost('h203', ip='128.114.2.203/24', defaultRoute="h203-eth0", mac='00:00:00:00:00:07')
+    h203 = self.addHost('h203', ip='128.114.2.203/24', defaultRoute="h203-eth23", mac='00:00:00:00:00:07')
     self.addLink(h203, Floor2Switch2, port1=23, port2=53)
-    h204 = self.addHost('h204', ip='128.114.2.204/24', defaultRoute="h204-eth0", mac='00:00:00:00:00:08')
+    h204 = self.addHost('h204', ip='128.114.2.204/24', defaultRoute="h204-eth24", mac='00:00:00:00:00:08')
     self.addLink(h204, Floor2Switch2, port1=24, port2=54)
 
     # Trusted Host
-    h_trust = self.addHost('h_trust', ip='192.47.38.109/24', defaultRoute="h_trust-eth0", mac='00:00:00:00:00:09')
-    self.addLink(h_trust, TrustedSwitch, port1=99, port2=200)
+    h_trust = self.addHost('h_trust', ip='192.47.38.109/24', defaultRoute="h_trust-eth99", mac='00:00:00:00:00:09')
+    self.addLink(h_trust, CoreSwitch, port1=99, port2=200)
 
     # Untrusted Host
-    h_untrust = self.addHost('h_untrust', ip='108.35.24.113/24', defaultRoute="h_untrust-eth0", mac='00:00:00:00:00:10')
+    h_untrust = self.addHost('h_untrust', ip='108.35.24.113/24', defaultRoute="h_untrust-eth98", mac='00:00:00:00:00:10')
     self.addLink(h_untrust, CoreSwitch, port1=98, port2=201)
     
     # LLM Server
-    h_server = self.addHost('h_server', ip='128.114.3.178/24', defaultRoute="h_server-eth0", mac='00:00:00:00:00:11')
+    h_server = self.addHost('h_server', ip='128.114.3.178/24', defaultRoute="h_server-eth17", mac='00:00:00:00:00:11')
     self.addLink(h_server, DataCenterSwitch, port1=17, port2=18)
 
-    discord = self.addHost('discord', ip='200.20.50.5/32', defaultRoute="discord-eth0", mac="00:00:00:00:00:ff")
-    self.addLink(discord, CoreSwitch, port1=123, port2=321)
+    # discord = self.addHost('discord', ip='200.20.50.5/32', defaultRoute="discord-eth0", mac="00:00:00:00:00:ff")
+    # self.addLink(discord, CoreSwitch, port1=123, port2=321)
 
 if __name__ == '__main__':
   # This part of the script is run when the script is executed
